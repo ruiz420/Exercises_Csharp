7425ae3a57881b60a83b714704c62ed68f846963
Your Name <roldanchakih@gmail.com>
2025-09-14 10:26:05 -0500

    se crea el ejercisio 5, numero multiplo de otro

---
+++ exercises/IsMultipleOfAnother/Program.cs
@@ -0,0 +1,20 @@
+﻿using shared;
+
+var response = String.Empty;
+
+do
+{
+    var a = ConsoleExtensions.GetInt("ingrese primer numero: ");
+    var b = ConsoleExtensions.GetInt("ingrese segundo numero: ");
+    if (a % b == 0)
+    {
+        Console.WriteLine($"{a} es multiplo de {b}  ");
+    }
+    else
+    { 
+        Console.WriteLine($"{a} No es multiplo de {b}");
+    }
+        Console.WriteLine("Desea Continuar [S/N]? ");
+    response = Console.ReadLine()!.ToUpper();
+
+} while (response == "S") ;
